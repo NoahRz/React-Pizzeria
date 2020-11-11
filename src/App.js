06a1b8aa181fc1939006db81882dc61184ce14a1
Noah Razafindrabe <noah.razafindrabe@outlook.com>
2020-11-11 22:07:55 +0100

    connected react to api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './index.css';
 import  {BrowserRouter as Router, Switch,  Route, Link, Redirect} from 'react-router-dom';
 import Home from './pages';
 import { Navbar, Sidebar} from './components';
+import axios from 'axios';
 
+async function makeGetRequest(url) {
+
+  let res = await axios.get(url);
+
+  let data = res.data;
+  
+  return data;
+}
+
+async function makePostRequest(url) {
+
+  let res = await axios.post(url, {
+    name : "fourth pizza",
+    description : "my fourth pizza"
+  });
+  console.log("2",res);
+  return res;
+}
 
 function App() {
+
+  const [tests, setTests] = useState([]);
+
+  useEffect(() => {
+    addTest();
+    //fetchTests();
+  }, [])
+
+  const fetchTests = () => {
+    makeGetRequest('http://localhost:3000/api/v1/pizzas')
+    .then((data) => setTests(data))
+    .catch((err) => console.log(err))
+  }
+
+  const addTest = () => {
+    makePostRequest('http://localhost:3000/api/v1/pizza')
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err))
+  }
 
   const [isOpen, setIsOpen] = useState(false);
 
@@ -13,18 +51,26 @@ function App() {
         setIsOpen(!isOpen);
     }
 
+  console.log(tests);
   return (
     <Router>
       <div className="App">
+        {/*<ul>
+          {
+            tests.map((value, index) => {
+              return <li key={index}>{value.name}</li>
+            })
+          }
+        </ul>*/}
         <Sidebar isOpen={isOpen} openHandle={openHandle}/>
         <Navbar openHandle={openHandle} />
         <Switch>
           <Route exact path="/" component={Home}/> 
           <Route path="/about"> {/* when we go to the path (route) "/about" we are gonna render the component "About"*/}
-            <div/>
+            <h1>Something1</h1>
           </Route>
-          <Route path="/shop">
-            <div />
+          <Route path="/order">
+            <h1>Something2</h1>
           </Route>
         </Switch>
       </div>
